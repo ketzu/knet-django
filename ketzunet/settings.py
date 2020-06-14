93403ade75e246d66506f7e25e63ab8dda9b2831
David <david.moedinger@ketzu.net>
2020-06-14 23:37:29 +0200

    serving static files from different dir as listing them...

--- ketzunet/settings.py
+++ ketzunet/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "public_static/")
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
 ]
