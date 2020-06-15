c3242e9aa39d31fe0dd8571bfbf8df0900946d11
David <david.moedinger@ketzu.net>
2020-06-15 20:59:00 +0200

    rework projects

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = JPEGField(upload_to='projects/', variations={'full': (None, None), 'show': {'width': 500, 'height': 500, 'crop': True}}, null=True)
-    link = models.CharField(max_length=500)
+    link = models.CharField(max_length=500, null=True)
     git = models.CharField(max_length=500, null=True)
 
     def __str__(self):
