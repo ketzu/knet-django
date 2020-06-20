458e2a98698ed97bc7f71d9e4d811bde36cff464
David <david.moedinger@ketzu.net>
2020-06-20 20:22:14 +0200

    Switch from JPEG field to StdImageField for projects

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-from stdimage import JPEGField
+from stdimage import StdImageField
 
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    image = JPEGField(upload_to='projects/', variations={'full': (None, None), 'show': {'width': 500, 'height': 500, 'crop': True}}, null=True)
+    image = StdImageField(upload_to='projects/', variations={'show': {'width': 285, 'height': 285, 'crop': True}}, null=True)
     link = models.CharField(max_length=500, null=True)
     git = models.CharField(max_length=500, null=True)
 
