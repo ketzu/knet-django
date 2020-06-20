7941ef9ac6ed9c201f16ea797b7d7652f8bf1182
David <david.moedinger@ketzu.net>
2020-06-20 19:39:32 +0200

    Added debug media serving

--- ketzunet/urls.py
+++ ketzunet/urls.py
@@ -27,3 +27,6 @@ urlpatterns = [
     path('projects/', include('projects.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
