99cf65c7ef31184cd5dd6c4d3f8ecd574e0d5473
David <david.moedinger@ketzu.net>
2020-06-20 23:03:30 +0200

    Image upload improved

--- photography/admin.py
+++ photography/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from photography.models import Gallery, Image
 
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('get_preview',)
 
-admin.site.register(Gallery)
+    def get_preview(self, obj):
+        from django.utils.html import mark_safe
+        return mark_safe('<img src="%s" height="100px"/>' % obj.picture.url)
+
+
+class ImageStacked(admin.StackedInline):
+    model = Image
+    extra = 20
+
+
+class GalleryAdmin(admin.ModelAdmin):
+    inlines = [ImageStacked]
+
+
+admin.site.register(Gallery, GalleryAdmin)
--- photography/models.py
+++ photography/models.py
@@ -23,11 +23,5 @@ class Image(models.Model):
     picture = JPEGField(upload_to='gallery/', variations={'full': (None, None), 'thumbnail': {'height': 200}, 'big_thumbnail': {'width': 360}}, null=True)
     in_gallery = models.ForeignKey(Gallery, null=True, on_delete=models.SET_NULL)
 
-
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ('get_preview',)
-
-    def get_preview(self, obj):
-        from django.utils.html import mark_safe
-        return mark_safe('<img src="%s" height="100px"/>' % obj.picture.url)
+    def __str__(self):
+        return self.picture.name
