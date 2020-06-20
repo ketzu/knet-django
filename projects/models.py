79fd9899f2dcea1fb4f2fa238a0edfc0100eb810
David <david.moedinger@ketzu.net>
2020-06-20 20:35:04 +0200

    Allow nulls for project strings

--- projects/models.py
+++ projects/models.py
@@ -8,8 +8,8 @@ class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = StdImageField(upload_to='projects/', variations={'show': {'width': 285, 'height': 285, 'crop': True}}, null=True)
-    link = models.CharField(max_length=500, null=True)
-    git = models.CharField(max_length=500, null=True)
+    link = models.CharField(max_length=500, null=True, blank=True)
+    git = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
